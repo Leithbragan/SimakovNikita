4d704274392e5e40747bdd933a038e7a0a5b84cf
Leithbragan <zsn_90@mail.ru>
2016-05-13 22:04:33 +0300

    fix

--- src/ru/kpfu/itis/group11506/homework/semestr/DataHandling.java
+++ src/ru/kpfu/itis/group11506/homework/semestr/DataHandling.java
@@ -4,22 +4,21 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
+import java.util.List;
 
 public class DataHandling {
-    
+
     private static int total;
     private static int numberOfMen;
     private static int numberOfWomen;
-    private static String[] m;
-    private static String[] w;
-    protected static String[] namesMen;
-    protected static String[] namesWomen;
-    protected static int[] idMen;
-    protected static int[] idWomen;
-    private static String[] selectionMen;
-    private static String[] selectionWomen;
-    protected static Map<Integer, List<Integer>> mapForMen = new HashMap<>();
-    protected static Map<Integer, List<Integer>> mapForWomen = new HashMap<>();
+    private static List<String> namesMen;
+    private static List<String> namesWomen;
+    private static List<Integer> idMen;
+    private static List<Integer> idWomen;
+    private static List<String> selectionMen;
+    private static List<String> selectionWomen;
+    private static Map<Integer, List<Integer>> mapForMen = new HashMap<>();
+    private static Map<Integer, List<Integer>> mapForWomen = new HashMap<>();
 
 
     public static String readInputFile() throws IOException {
@@ -36,157 +35,150 @@ public class DataHandling {
         return string;
     }
 
-    public static void helper(String string) {
-        int i = 0;
+    public static String elementStringArray(String string, int numberElement) {
         String[] items = string.split(";\r\n");
-        String[] newitems = items[0].split(", ");
-        total = newitems.length;
-        for (int n = 0; n < newitems.length; n++) {
-            char[] b = newitems[n].toCharArray();
-            if (b[b.length - 1] == 'm') {
-                i++;
-            }
-        }
-        numberOfMen = i;
-        numberOfWomen = total - i;
+        return items[numberElement];
     }
 
-    public static void fileDivision(String string) {
-        String[] men = new String[numberOfMen];
-        String[] women = new String[numberOfWomen];
-        int j = 0, i = 0;
-        String[] items = string.split(";\r\n");
-        String[] newitems = items[0].split(", ");
-        for (int n = 0; n < newitems.length; n++) {
-            char[] b = newitems[n].toCharArray();
-            if (b[b.length - 1] == 'm') {
-                men[i] = newitems[n];
-                i++;
-            } else {
-                women[j] = newitems[n];
-                j++;
-            }
-        }
-        m = men;
-        w = women;
-    }
-
-    public static void createBaze(String string) {
-        //пременные для мужчин
-        String menString = "";
-        for (int i = 0; i < m.length; i++) {
-            menString += m[i] + " ";
-        }
-        String[] ma = menString.split(" ");
-        String[] menName = new String[m.length];
-        int[] idM = new int[m.length];
-        //переменные для женщин
-        String womenString = "";
-        for (int i = 0; i < m.length; i++) {
-            womenString += w[i] + " ";
-        }
-        String[] wa = womenString.split(" ");
-        String[] womenName = new String[w.length];
-        //переменные для предпочтений
-        String[] menS = new String[m.length];
-        String[] womenS = new String[w.length];
+    public static String[] sortArray() throws IOException {
         String sort = "";
-        String[] items = string.split(";\r\n");
-        String[] sortFirst = items[1].split("\r\n");
-        String[] sortSecond;
+        String[] sortFirst = elementStringArray(readInputFile(), 1).split("\r\n");
         sortFirst[0] = sortFirst[0].replace("sort = ", "");
         for (int z = 0; z < total; z++) {
             sort += sortFirst[z] + " ";
         }
         sort = sort.replace("{", "");
         sort = sort.replace("}", "");
-        sortSecond = sort.split(" ");
-        //заполнение массивов для мужчин
-        int k = 0;
-        for (int i = 0; i < m.length * 3 - 2; i = i + 3) {
-            menName[k] = ma[i];
-            k++;
-        }
-        namesMen = menName;
-        k = 0;
-        for (int l = 1; l < m.length * 3 - 1; l = l + 3) {
-            idM[k] = Integer.parseInt(ma[l]);
-            k++;
-        }
-        idMen = idM;
-        k = 0;
-        for (int z = 0; z < total * 2 - 3; z = z + 2) {
-            int gh = Integer.parseInt(sortSecond[z]);
-            if (gh == idM[k]) {
-                menS[k] = sortSecond[z + 1];
-                k++;
-            }
-        }
-        selectionMen = menS;
-        //заполнение массивов для женщин
-        k = 0;
-        for (int i = 0; i < w.length * 3 - 2; i = i + 3) {
-            womenName[k] = wa[i];
-            k++;
-        }
-        namesWomen = womenName;
-        k = 0;
-        int[] idW = new int[numberOfWomen];
-        for (int l = 1; l < w.length * 3 - 1; l = l + 3) {
-            idW[k] = Integer.parseInt(wa[l]);
-            k++;
-        }
-        idWomen = idW;
-        k = 0;
-        for (int z = 0; z < total * 2 - 1; z = z + 2) {
-            int gh = Integer.parseInt(sortSecond[z]);
-            if (gh == idW[k]) {
-                womenS[k] = sortSecond[z + 1];
-                k++;
-                Map<Integer, List<Integer>> mapWomen = new HashMap<>();
-            }
-        }
-        selectionWomen = womenS;
+        String[] sortArray = sort.split(" ");
+        return sortArray;
     }
 
-    public static ArrayList<Integer> transfer(String selection) {
-        List<Integer> listArray = new ArrayList<>();
-        String[] helpString = selection.split(",");
-        for (int j = 0; j < m.length; j++) {
-            listArray.add(Integer.parseInt(helpString[j]));
+    public static String createString(String[] stringArray) {
+        String string = "";
+        for (int i = 0; i < numberOfMen; i++) {
+            string += stringArray[i] + " ";
+        }
+        return string;
+    }
 
+    public static String[] initialization(int size, char gender) throws IOException {
+        String[] items = elementStringArray(readInputFile(), 0).split(", ");
+        int i = 0;
+        String[] strings = new String[size];
+        for (int n = 0; n < items.length; n++) {
+            char[] b = items[n].toCharArray();
+            if (b[b.length - 1] == gender) {
+                strings[i] = items[n];
+                i++;
+            }
+
+        }
+        return strings;
+    }
+
+    public static List<String> names(String[] clients, int size) {
+        List<String> namesClients = new ArrayList<>();
+        for (int i = 0; i < size * 3 - 2; i = i + 3) {
+            namesClients.add(clients[i]);
+        }
+        return namesClients;
+    }
+
+    public static List<Integer> ids(String[] clients, int size) {
+        List<Integer> idsClients = new ArrayList<>();
+        for (int l = 1; l < size * 3 - 1; l = l + 3) {
+            idsClients.add(Integer.parseInt(clients[l]));
+        }
+        return idsClients;
+    }
+
+    private static char lastLetter() throws IOException {
+        String data = elementStringArray(readInputFile(), 0);
+        char[] dataCharArray = data.toCharArray();
+        return dataCharArray[dataCharArray.length - 1];
+    }
+
+    public static List<String> sortA(String letter, String[] sortArray) throws IOException {
+        List<String> selectiveSorting = new ArrayList<>();
+        int condition, k = 0;
+        if (Objects.equals(letter, lastLetter())) {
+            condition = 1;
+        } else {
+            condition = 3;
+        }
+        for (int z = 0; z < total * 2 - condition; z = z + 2) {
+            int gh = Integer.parseInt(sortArray[z]);
+            if (gh == idMen.get(k)) {
+                selectiveSorting.add(sortArray[z+1]);
+                k++;
+            }
+        }
+        return selectiveSorting;
+    }
+
+    public static void helper() throws IOException {
+        String[] newitems = elementStringArray(readInputFile(), 0).split(", ");
+        total = newitems.length;
+        for (int n = 0; n < newitems.length; n++) {
+            char[] b = newitems[n].toCharArray();
+            if (b[b.length - 1] == 'm') {
+                numberOfMen++;
+            } else {
+                numberOfWomen++;
+            }
+        }
+    }
+
+    public static void createBaze() throws IOException {
+        //пременные для мужчин
+        String menString = createString(initialization(numberOfMen, 'm'));
+        String[] ma = menString.split(" ");
+        //переменные для женщин
+        String womenString = createString(initialization(numberOfWomen, 'f'));
+        String[] wa = womenString.split(" ");
+        //переменные для предпочтений
+        String[] sortSecond = sortArray();
+        //заполнение массивов для мужчин
+        namesMen = names(ma, numberOfMen);
+        idMen = ids(ma, numberOfMen);
+        selectionMen = sortA("m", sortSecond);
+        //заполнение массивов для женщин
+        namesWomen = names(wa, numberOfWomen);
+        idWomen = ids(wa, numberOfWomen);
+        selectionWomen = sortA("f", sortSecond);
+    }
+
+    public static ArrayList<Integer> transfer(String selection, int size) {
+        List<Integer> listArray = new ArrayList<>();
+        String[] oneSelectionArray = selection.split(",");
+        for (int j = 0; j < numberOfMen; j++) {
+            listArray.add(Integer.parseInt(oneSelectionArray[j]));
         }
         return (ArrayList<Integer>) listArray;
     }
 
     public static void putInMap() {
-        Map<Integer, List<Integer>> mapMen = new HashMap<>();
-        Map<Integer, List<Integer>> mapWomen = new HashMap<>();
-        for (int i = 0; i < m.length; i++) {
-            mapMen.put(idMen[i], transfer(selectionMen[i]));
-
+        for (int i = 0; i < numberOfMen; i++) {
+            mapForMen.put(idMen.get(i), transfer(selectionMen.get(i), numberOfMen));
         }
-        mapForMen = mapMen;
-        for (int i = 0; i < w.length; i++) {
-            mapWomen.put(idWomen[i], transfer(selectionWomen[i]));
-        }
-        mapForWomen = mapWomen;
-    }
 
-    private static void oops() {
-        for (int i = 0; i < m.length; i++) {
-            m1.add(idMen[i]);
+        for (int i = 0; i < numberOfWomen; i++) {
+            mapForWomen.put(idWomen.get(i), transfer(selectionWomen.get(i),numberOfWomen));
         }
     }
-
-    private static List<Integer> m1 = new LinkedList<>();
 
     public static void run() throws IOException {
-        helper(readInputFile());
-        fileDivision(readInputFile());
-        createBaze(readInputFile());
+        helper();
+        createBaze();
         putInMap();
-        oops();
-        Magic.run(m1, mapForMen, mapForWomen, namesMen, namesWomen);
+        Magic.run(idMen, mapForMen, mapForWomen, namesMen, namesWomen);
+    }
+
+    public static List<Integer> getIdMen() {
+        return idMen;
+    }
+
+    public static List<Integer> getIdWomen() {
+        return idWomen;
     }
 }
